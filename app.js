f08d6895aad870168b1ab1e2bc4ffb1f276040aa
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-11 19:21:18 -0800

    add gif

---
+++ app.js
@@ -0,0 +1,11 @@
+const Typed = require("typed.js");
+
+var typed6 = new Typed('#typed6', {
+    strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
+    typeSpeed: 40,
+    backSpeed: 0,
+    loop: true
+  });
+
+return typed6;
+
